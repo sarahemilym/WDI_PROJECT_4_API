297c43e81d039d2ce5d3b12150aab7decc9b5903
sarahemilym <sarahemendham@gmail.com>
2017-02-21 10:10:57 +0000

    added user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/:id', to: 'users#show'
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216121215) do
+ActiveRecord::Schema.define(version: 20170220112933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "musics", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_musics_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -22,4 +29,5 @@ ActiveRecord::Schema.define(version: 20170216121215) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "musics", "users"
 end
